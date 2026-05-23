7c5c2e278998fd37941c855da43aeaee37b5cf2e
Francisco Martins <francisco.andre.carvalho.martins@tecnico.ulisboa.pt>
2026-05-24 00:04:20 +0100

    somewhat useless code, that goes on the right direction

--- clippy_lints/src/methods/redundant_idempotent_calls.rs
+++ clippy_lints/src/methods/redundant_idempotent_calls.rs
@@ -1,9 +1,13 @@
-use clippy_utils::sym;
 use rustc_lint::LateContext;
 use rustc_middle::mir::Body;
-use rustc_mir_dataflow::JoinSemiLattice;
+use rustc_middle::mir::StatementKind::{Assign, StorageLive};
+use rustc_middle::mir::Local;
+use rustc_middle::mir::BasicBlock;
+use rustc_index::IndexVec;
+//use rustc_mir_dataflow::JoinSemiLattice;
+use std::collections::HashMap;
 use rustc_span::Symbol;
-
+use clippy_utils::{fn_has_unsatisfiable_preds, sym};
 use super::REDUNDANT_IDEMPOTENT_CALLS;
 
 pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, body: &'tcx Body<'tcx>) {
@@ -17,15 +21,50 @@ pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, body: &'tcx Body<'tcx>) {
         MethodState::Bottom,
         &body.local_decls,
     );
-
+    let mut terminators =  Vec::new();
+    //this map keeps the record of the successors for each block, so as to determine the predecessors of each block
+    let mut block_successors_map = HashMap::new();
+    let mut block_predecessors_map = HashMap::new();
     // Iterate over basic blocks in order
+    println!("the entire mir, ugly, {:#?}",body);
     for (_bb, bbdata) in body.basic_blocks.iter_enumerated() {
-
-        for statement in &bbdata.statements{
-
-        }
-
         let terminator = bbdata.terminator();
+        block_successors_map.insert(_bb,terminator.successors());
+        terminators.push(terminator);
+        for successor in terminator.successors(){
+            println!("these are the successors {:#?}", successor);
+            if(block_predecessors_map.contains_key(&successor)){
+                let mut predecessors: Vec<&BasicBlock>= block_predecessors_map.get(&successor).unwrap_or_else(
+                    panic!("you suck")
+                );
+                predecessors.push(_bb);
+            } else{
+                let mut predecessors = Vec::new();
+                predecessors.push(_bb);
+                block_predecessors_map.insert(successor,predecessors);
+            }
+            
+        }
+        //update existing symbolds given the predecessors
+        println!("this is the current block {:#?}",_bb);
+        //update/create new symbols according to the current statements
+        for statement in &bbdata.statements{
+            match &statement.kind{
+                Assign(box (place, rvalue))=>{
+                    println!("this is an assignment, you suck {:#?} {:#?}",place,rvalue);
+                }
+                StorageLive(sucks)=>{
+                    println!("this is a storage live {:#?}",sucks);
+                }
+                _ =>{
+                    println!("dont care");
+                }
+            }
+        }
+        
+
+        
+        println!("this the terminator {:#?}",terminator);
     }
 
 }
@@ -38,24 +77,24 @@ enum MethodState {
     Top,
 }
 
-// Logic:
-// update if needed the join value
-// https://doc.rust-lang.org/nightly/nightly-rustc/src/rustc_mir_dataflow/framework/lattice.rs.html#107-121
-// e.g.
-// self=Top, other=Applied(to_lowercase) ---> no change is needed, it remains Top
-impl JoinSemiLattice for MethodState {
-    fn join(&mut self, other: &Self) -> bool {
-        let result = match (&*self, other) {
-            (Self::Top, _) | (_, Self::Bottom) => return false,
-            (Self::Applied(a), Self::Applied(b)) if a == b => return false,
-            (Self::Bottom, Self::Applied(x)) => Self::Applied(x.clone()),
-            _=> Self::Top,
-        };
-        // self has changed
-        *self = result;
-        true
-    }
-}
+// // Logic:
+// // update if needed the join value
+// // https://doc.rust-lang.org/nightly/nightly-rustc/src/rustc_mir_dataflow/framework/lattice.rs.html#107-121
+// // e.g.
+// // self=Top, other=Applied(to_lowercase) ---> no change is needed, it remains Top
+// impl JoinSemiLattice for MethodState {
+//     fn join(&mut self, other: &Self) -> bool {
+//         let result = match (&*self, other) {
+//             (Self::Top, _) | (_, Self::Bottom) => return false,
+//             (Self::Applied(a), Self::Applied(b)) if a == b => return false,
+//             (Self::Bottom, Self::Applied(x)) => Self::Applied(x.clone()),
+//             _=> Self::Top,
+//         };
+//         // self has changed
+//         *self = result;
+//         true
+//     }
+// }
 
 fn is_idempotent(name: Symbol) -> bool {
     matches!(name,
