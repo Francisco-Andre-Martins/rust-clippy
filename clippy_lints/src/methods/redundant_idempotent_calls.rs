e2ec37ba12fff6f0708a6a8600aa35d5700a0b72
Guilherme Silva <guilherme.d.silva@tecnico.ulisboa.pt>
2026-05-23 11:57:57 +0100

    started idemp check

--- clippy_lints/src/methods/redundant_idempotent_calls.rs
+++ clippy_lints/src/methods/redundant_idempotent_calls.rs
@@ -1,20 +1,55 @@
-use rustc_hir::{Body, FnDecl, intravisit::FnKind};
+use clippy_utils::sym;
+use rustc_hir::{Body, ExprKind, StmtKind};
 use rustc_lint::LateContext;
-use rustc_span::Span;
+use rustc_span::Symbol;
+use std::collections::HashMap;
+
+
+use super::REDUNDANT_IDEMPOTENT_CALLS
 
 pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, body: &'tcx Body<'tcx>) {
-    walk_block(cx, &body.value);
+
+    // If the body of the funciton is a block
+    if let ExprKind::Block(block, _) = &body.value.kind {
+        let mut map = HashMap::new();
+        walk_block(cx, block, &mut map);
+    }
 }
 
+fn walk_block<'tcx>(
+    cx: &LateContext<'tcx>,
+    block: &'tcx rustc_hir::Block<'tcx>,
+    map: &mut HashMap<HirId, Symbol>,
+) {
+    for stmt in block.stmts {
+        match &stmt.kind {
+            StmtKind::Let(local) => check_let(local, map),
+            StmtKind::Semi(expr) => check_expr(cx, expr, map),
+            _ => {},
+        }
+    }
 
-use super::REDUNDANT_IDEMPOTENT_CALLS;
+    if let Some(expr) = block.expr {
+        check_expr(cx, expr, map);
+    }
+}
 
-pub(super) fn check_fn<'tcx>{
-    cx: $LateContext<'tcx>,
-    kind: FnKind<'tcx>,
-    decl: &'tcx FnDecl<'tcx>,
-    body: &'tcx Body<'tcx>,
-    span: Span
-} {
-    todo!()
+fn check_let<'tcx>(
+    local: &'tcx rustc_hir::LetStmt<'tcx>,
+    map: &mut HashMap<HirId, Symbol>,
+) {
+    if let Some(init) = local.init
+        && let ExprKind::MethodCall(mehtod, _receiver, args, _) = &init.kind
+        && args.is_empty()
+        && is_idempotent(method.ident.name)
+        && let PatKind::Binding(_, hir_id, _, _) = local.pat.kind
+    {
+        map.insert(hir_id, method.ident.name);
+    }
+}
+
+fn is_idempotent(name: Symbol) -> bool {
+    matches!(name,
+        sym::to_lowercase | sym::to_uppercase | sym::trim | sym::abs
+    )
 }
