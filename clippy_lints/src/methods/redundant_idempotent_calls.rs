4192f2a71c9324a9753728f58de6e49f0da4b4b0
Francisco Martins <francisco.andre.carvalho.martins@tecnico.ulisboa.pt>
2026-05-28 21:05:37 +0100

    formatting and reorganization

--- clippy_lints/src/methods/redundant_idempotent_calls.rs
+++ clippy_lints/src/methods/redundant_idempotent_calls.rs
@@ -10,16 +10,19 @@ use rustc_lint::LateContext;
 use rustc_span::Symbol;
 
 use super::REDUNDANT_IDEMPOTENT_CALLS;
+
+// Checks all function code
 pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, body: &'tcx Body<'tcx>) {
     // If the body of the funciton is a block
     if let ExprKind::Block(block, _) = &body.value.kind {
         // tracks which local variables have already had an idempotent method applied
+        // and the arguments used with them
         // HirId : name of the method that was applied
         let mut map = FxIndexMap::default();
         walk_block(cx, block, &mut map);
     }
 }
-
+// Checks blocks of code (ifs, loops, match arms, etc...)
 fn walk_block<'tcx>(
     cx: &LateContext<'tcx>,
     block: &'tcx rustc_hir::Block<'tcx>,
@@ -34,105 +37,15 @@ fn walk_block<'tcx>(
             _ => {},
         }
     }
-
+    // Covers the final expression the block evaluates to
     if let Some(expr) = block.expr {
         return check_expr(cx, expr, map);
     }
     None
 }
+// Checking functions for language elements
 
-fn invalidate_left_value<'tcx>(
-    expr: &'tcx rustc_hir::Expr<'tcx>,
-    map: &mut FxIndexMap<HirId, (Symbol, &[rustc_hir::Expr<'_>])>,
-) {
-    if let Some(hir_id) = path_to_local(expr) {
-        map.shift_remove(&hir_id);
-
-        // if any entry that its args reference the variable
-        map.retain(|_, (_, args)| !args.iter().any(|arg| path_to_local(arg) == Some(hir_id)));
-    }
-}
-
-fn check_let<'tcx>(
-    cx: &LateContext<'tcx>,
-    local: &'tcx rustc_hir::LetStmt<'tcx>,
-    map: &mut FxIndexMap<HirId, (Symbol, &'tcx [rustc_hir::Expr<'tcx>])>,
-) {
-    if let Some(init) = local.init {
-        // inrelevant and inserted by the compiler
-        let expr = strip_parens_and_temps(init);
-
-        if let ExprKind::MethodCall(method, receiver, args, _) = &expr.kind
-            && is_idempotent(cx, expr, method.ident.name)
-        {
-            check_method_call(cx, expr, method, receiver, args, map);
-            // record the new binding if it is a simple identifier
-            if let PatKind::Binding(_, hir_id, _, _) = local.pat.kind {
-                map.insert(hir_id, (method.ident.name, args));
-            }
-        } else {
-            if let PatKind::Binding(_, hir_id, _, _) = local.pat.kind {
-                // try to inherit the alias symbol otherwise we check the expr
-                if !try_inherit_alias(expr, hir_id, map) {
-                    if let Some((symbol, args)) = check_expr(cx, expr, map) {
-                        map.insert(hir_id, (symbol, args));
-                    }
-                }
-            } else {
-                check_expr(cx, expr, map);
-            }
-        }
-    }
-
-    if let Some(els) = local.els {
-        walk_block(cx, els, map);
-    }
-}
-
-fn is_idempotent<'tcx>(cx: &LateContext<'tcx>, expr: &rustc_hir::Expr<'tcx>, name: Symbol) -> bool {
-    if !matches!(
-        name,
-        sym::trim
-            | sym::trim_start
-            | sym::trim_end
-            | sym::to_lowercase
-            | sym::to_uppercase
-            | sym::to_ascii_lowercase
-            | sym::to_ascii_uppercase
-            | sym::abs
-            | sym::floor
-            | sym::ceil
-            | sym::round
-            | sym::signum
-            | sym::max
-            | sym::min
-            | sym::clamp
-            | sym::to_vec
-            | sym::and
-            | sym::or
-    ) {
-        return false;
-    }
-    // check if the method comes from the stdlib
-    if let Some(def_id) = cx.typeck_results().type_dependent_def_id(expr.hir_id) {
-        let crate_name = cx.tcx.crate_name(def_id.krate);
-        crate_name == sym::std || crate_name == sym::alloc || crate_name == sym::core
-    } else {
-        false
-    }
-}
-
-fn path_to_local(expr: &rustc_hir::Expr<'_>) -> Option<HirId> {
-    if let ExprKind::Path(QPath::Resolved(None, path)) = &expr.kind
-        && let Res::Local(hir_id) = path.res
-    {
-        Some(hir_id)
-    } else {
-        None
-    }
-}
-
-// returns true if there is an insertion into the map
+// Checks any expression
 fn check_expr<'tcx>(
     cx: &LateContext<'tcx>,
     expr: &'tcx rustc_hir::Expr<'tcx>,
@@ -192,65 +105,44 @@ fn check_expr<'tcx>(
         _ => None,
     }
 }
-fn check_func_args<'tcx>(
+// Checks a let expression
+fn check_let<'tcx>(
     cx: &LateContext<'tcx>,
-    args: &'tcx [rustc_hir::Expr<'tcx>],
+    local: &'tcx rustc_hir::LetStmt<'tcx>,
     map: &mut FxIndexMap<HirId, (Symbol, &'tcx [rustc_hir::Expr<'tcx>])>,
-) -> Option<(Symbol, &'tcx [rustc_hir::Expr<'tcx>])> {
-    for arg in args {
-        if let ExprKind::AddrOf(_, Mutability::Mut, inner) = &arg.kind
-            && let Some(hir_id) = path_to_local(inner)
-        {
-            map.shift_remove(&hir_id);
-        } else {
-            check_expr(cx, arg, map);
-        }
-    }
-    None
-}
+) {
+    if let Some(init) = local.init {
+        // inrelevant and inserted by the compiler
+        let expr = strip_parens_and_temps(init);
 
-fn is_expr_safe_to_compare(expr: &rustc_hir::Expr<'_>) -> bool {
-    match &expr.kind {
-        ExprKind::Lit(_) | ExprKind::Path(_) => true,
-        ExprKind::Unary(_, inner) => is_expr_safe_to_compare(inner),
-        // to cover constructors just like Some(1), Ok(2), None (but only literals)
-        ExprKind::Call(func, args) => {
-            matches!(func.kind, ExprKind::Path(_)) && args.iter().all(is_expr_safe_to_compare)
-        },
-        _ => false,
-    }
-}
-
-fn are_args_equal<'tcx>(
-    cx: &LateContext<'tcx>,
-    args1: &'tcx [rustc_hir::Expr<'tcx>],
-    args2: &'tcx [rustc_hir::Expr<'tcx>],
-) -> bool {
-    for (arg1, arg2) in args1.iter().zip(args2.iter()) {
-        let const_context = ConstEvalCtxt::new(cx);
-        if let Some(const_1) = const_context.eval(arg1)
-            && let Some(const_2) = const_context.eval(arg2)
+        if let ExprKind::MethodCall(method, receiver, args, _) = &expr.kind
+            && is_idempotent(cx, expr, method.ident.name)
         {
-            if const_1 != const_2 {
-                return false;
-            }
-        } else if let Some(id1) = path_to_local(arg1)
-            && let Some(id2) = path_to_local(arg2)
-        {
-            if id1 != id2 {
-                return false;
-            }
-        } else if is_expr_safe_to_compare(arg1) && is_expr_safe_to_compare(arg2) {
-            if !clippy_utils::SpanlessEq::new(cx).eq_expr(arg1.span.ctxt(), arg1, arg2) {
-                return false;
+            check_method_call(cx, expr, method, receiver, args, map);
+            // record the new binding if it is a simple identifier
+            if let PatKind::Binding(_, hir_id, _, _) = local.pat.kind {
+                map.insert(hir_id, (method.ident.name, args));
             }
         } else {
-            return false;
+            if let PatKind::Binding(_, hir_id, _, _) = local.pat.kind {
+                // try to inherit the alias symbol otherwise we check the expr
+                if !try_inherit_alias(expr, hir_id, map) {
+                    if let Some((symbol, args)) = check_expr(cx, expr, map) {
+                        map.insert(hir_id, (symbol, args));
+                    }
+                }
+            } else {
+                check_expr(cx, expr, map);
+            }
         }
     }
-    true
+
+    if let Some(els) = local.els {
+        walk_block(cx, els, map);
+    }
 }
 
+// Checks for method calls
 fn check_method_call<'tcx>(
     cx: &LateContext<'tcx>,
     expr: &'tcx rustc_hir::Expr<'tcx>,
@@ -368,6 +260,7 @@ fn check_if<'tcx>(
     None
 }
 
+// Checks assignments
 fn check_assign<'tcx>(
     cx: &LateContext<'tcx>,
     left_value: &'tcx rustc_hir::Expr<'tcx>,
@@ -388,6 +281,7 @@ fn check_assign<'tcx>(
     None
 }
 
+// Checks loops
 fn check_loop<'tcx>(
     cx: &LateContext<'tcx>,
     block: &'tcx rustc_hir::Block<'tcx>,
@@ -407,6 +301,7 @@ fn check_loop<'tcx>(
     None
 }
 
+//Checks matches
 fn check_match<'tcx>(
     cx: &LateContext<'tcx>,
     arms: &'tcx [rustc_hir::Arm<'tcx>],
@@ -440,6 +335,7 @@ fn check_match<'tcx>(
     None
 }
 
+// Checks closures
 fn check_closure<'tcx>(
     cx: &LateContext<'tcx>,
     closure: &'tcx rustc_hir::Closure<'tcx>,
@@ -452,6 +348,130 @@ fn check_closure<'tcx>(
         }
     }
     None
+}
+
+// Auxiliary functions
+
+// Removes a value from the tracked list
+fn invalidate_left_value<'tcx>(
+    expr: &'tcx rustc_hir::Expr<'tcx>,
+    map: &mut FxIndexMap<HirId, (Symbol, &[rustc_hir::Expr<'_>])>,
+) {
+    if let Some(hir_id) = path_to_local(expr) {
+        map.shift_remove(&hir_id);
+
+        // if any entry that its args reference the variable
+        map.retain(|_, (_, args)| !args.iter().any(|arg| path_to_local(arg) == Some(hir_id)));
+    }
+}
+
+// This is a known list of idempotent functions, that can either be expanded or replaced by some
+// primitive static analysis.
+fn is_idempotent<'tcx>(cx: &LateContext<'tcx>, expr: &rustc_hir::Expr<'tcx>, name: Symbol) -> bool {
+    if !matches!(
+        name,
+        sym::trim
+            | sym::trim_start
+            | sym::trim_end
+            | sym::to_lowercase
+            | sym::to_uppercase
+            | sym::to_ascii_lowercase
+            | sym::to_ascii_uppercase
+            | sym::abs
+            | sym::floor
+            | sym::ceil
+            | sym::round
+            | sym::signum
+            | sym::max
+            | sym::min
+            | sym::clamp
+            | sym::to_vec
+            | sym::and
+            | sym::or
+    ) {
+        return false;
+    }
+    // check if the method comes from the stdlib
+    if let Some(def_id) = cx.typeck_results().type_dependent_def_id(expr.hir_id) {
+        let crate_name = cx.tcx.crate_name(def_id.krate);
+        crate_name == sym::std || crate_name == sym::alloc || crate_name == sym::core
+    } else {
+        false
+    }
+}
+
+// Resolves an expression into its respective HirId
+fn path_to_local(expr: &rustc_hir::Expr<'_>) -> Option<HirId> {
+    if let ExprKind::Path(QPath::Resolved(None, path)) = &expr.kind
+        && let Res::Local(hir_id) = path.res
+    {
+        Some(hir_id)
+    } else {
+        None
+    }
+}
+
+// Checks if any mutable references are passed down a function and if so, remove
+// those variables from the tracking Map
+fn check_func_args<'tcx>(
+    cx: &LateContext<'tcx>,
+    args: &'tcx [rustc_hir::Expr<'tcx>],
+    map: &mut FxIndexMap<HirId, (Symbol, &'tcx [rustc_hir::Expr<'tcx>])>,
+) -> Option<(Symbol, &'tcx [rustc_hir::Expr<'tcx>])> {
+    for arg in args {
+        if let ExprKind::AddrOf(_, Mutability::Mut, inner) = &arg.kind
+            && let Some(hir_id) = path_to_local(inner)
+        {
+            map.shift_remove(&hir_id);
+        } else {
+            check_expr(cx, arg, map);
+        }
+    }
+    None
+}
+
+fn is_expr_safe_to_compare(expr: &rustc_hir::Expr<'_>) -> bool {
+    match &expr.kind {
+        ExprKind::Lit(_) | ExprKind::Path(_) => true,
+        ExprKind::Unary(_, inner) => is_expr_safe_to_compare(inner),
+        // to cover constructors just like Some(1), Ok(2), None (but only literals)
+        ExprKind::Call(func, args) => {
+            matches!(func.kind, ExprKind::Path(_)) && args.iter().all(is_expr_safe_to_compare)
+        },
+        _ => false,
+    }
+}
+
+// In the case of some functions, they are idempotent as long as their arguments
+// are used in the same manner
+fn are_args_equal<'tcx>(
+    cx: &LateContext<'tcx>,
+    args1: &'tcx [rustc_hir::Expr<'tcx>],
+    args2: &'tcx [rustc_hir::Expr<'tcx>],
+) -> bool {
+    for (arg1, arg2) in args1.iter().zip(args2.iter()) {
+        let const_context = ConstEvalCtxt::new(cx);
+        if let Some(const_1) = const_context.eval(arg1)
+            && let Some(const_2) = const_context.eval(arg2)
+        {
+            if const_1 != const_2 {
+                return false;
+            }
+        } else if let Some(id1) = path_to_local(arg1)
+            && let Some(id2) = path_to_local(arg2)
+        {
+            if id1 != id2 {
+                return false;
+            }
+        } else if is_expr_safe_to_compare(arg1) && is_expr_safe_to_compare(arg2) {
+            if !clippy_utils::SpanlessEq::new(cx).eq_expr(arg1.span.ctxt(), arg1, arg2) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    true
 }
 
 fn try_inherit_alias<'a>(
